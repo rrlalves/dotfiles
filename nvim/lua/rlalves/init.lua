7ebf851763e395951a1cc7cfa63644ae5648bb00
Roger Luiz Alves <roger.alves@dzsi.com>
2023-05-10 13:57:41 -0300

    Add neovim configs

---
+++ nvim/lua/rlalves/init.lua
@@ -0,0 +1,6 @@
+require("rlalves.set")
+require("rlalves.remap")
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
---
+++ nvim/lua/rlalves/remap.lua
@@ -0,0 +1,28 @@
+vim.keymap.set("n", "<leader>qq", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
+vim.keymap.set("n", "<F5>", [[:lua vim.lsp.buf.format()<cr> <bar> :%s/\s\+$//e<cr>]])
+
+vim.keymap.set("n", "<leader>m", [[:enew<cr>]])
+vim.keymap.set("n", "<TAB>", [[:bnext<cr>]])
+vim.keymap.set("n", "<S-TAB>", [[:bprevious<cr>]])
+
+--vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+--vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+--vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+--vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+--
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>q", [[:bd<cr>]])
